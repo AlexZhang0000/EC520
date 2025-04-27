aefb6a90a3637ba59e835eb15e15679598ce6b82
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-27 09:12:34 -0400

    Update dataloader.py

--- image_segmentation/dataloader.py
+++ image_segmentation/dataloader.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-from torchvision.datasets import ADE20K
+from torchvision.datasets.ade20k import ADE20K
 import torchvision.transforms as T
 from PIL import Image
 
