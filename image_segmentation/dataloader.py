b30df3917cd9ac8314da6a1e05b6549a7d951d65
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-27 09:06:00 -0400

    Update dataloader.py

--- image_segmentation/dataloader.py
+++ image_segmentation/dataloader.py
@@ -2,13 +2,21 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-from torchvision.datasets import OxfordIIITPet
+from torchvision.datasets import ADE20K
 import torchvision.transforms as T
 from PIL import Image
 
-class OxfordPetsDataset(torch.utils.data.Dataset):
-    def __init__(self, root, split='trainval'):
-        self.dataset = OxfordIIITPet(root=root, split=split, target_types='segmentation', download=True)
+# 类别映射
+ADE20K_VALID_CLASSES = {
+    2: 1,   # building
+    12: 2,  # tree
+    14: 3   # person
+}
+BACKGROUND_LABEL = 0
+
+class ADE20KSubsetDataset(torch.utils.data.Dataset):
+    def __init__(self, root, mode='train'):
+        self.dataset = ADE20K(root=root, split=mode, target_types='segmentation', download=True)
 
         self.input_transform = T.Compose([
             T.Resize((256, 256)),
@@ -29,19 +37,20 @@ class OxfordPetsDataset(torch.utils.data.Dataset):
         mask = mask.resize((256, 256), Image.NEAREST)
         mask = np.array(mask)
 
-        # Oxford Pets 原本的mask中
-        # 0 = 背景
-        # 1 = 宠物
-        # 2 = 边界
-        mask = torch.from_numpy(mask).long()
+        # 重新映射标签
+        new_mask = np.ones_like(mask) * BACKGROUND_LABEL
+        for orig_class, new_class in ADE20K_VALID_CLASSES.items():
+            new_mask[mask == orig_class] = new_class
+
+        mask = torch.from_numpy(new_mask).long()
 
         return img, mask
 
-def get_loader(root, batch_size=16, mode='train', shuffle=True, num_workers=2):
-    split = 'trainval' if mode == 'train' else 'test'
-    dataset = OxfordPetsDataset(root=root, split=split)
+def get_loader(root, batch_size=8, mode='train', shuffle=True, num_workers=2):
+    dataset = ADE20KSubsetDataset(root=root, mode=mode)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=(mode=='train'), num_workers=num_workers)
     return loader
+
 
 
 
