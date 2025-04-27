346c797db38abf8f1b01af972571d5b8418521d0
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-27 09:05:04 -0400

    Update config.py

--- image_segmentation/config.py
+++ image_segmentation/config.py
@@ -2,16 +2,16 @@ import torch
 import os
 
 class Config:
-    project_name = 'image_segmentation_oxfordpets'
+    project_name = 'image_segmentation_ade20k_distortion'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     seed = 42
 
     # Dataset
-    data_root = './Data/OxfordPets'
-    num_classes = 3  # ✅ 背景(0)、宠物(1)、边界(2)
+    data_root = './Data/ADE20K'
+    num_classes = 4  # 背景、建筑、树、人
 
     # Training
-    batch_size = 16
+    batch_size = 8
     num_epochs = 200
     learning_rate = 1e-3
     weight_decay = 1e-4
