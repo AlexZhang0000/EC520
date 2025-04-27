b303d20e611f69566b365baa3b61146cd139c39c
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-27 09:35:29 -0400

    Update dataloader.py

--- image_segmentation/dataloader.py
+++ image_segmentation/dataloader.py
@@ -1,62 +1,57 @@
 import os
 import numpy as np
 import torch
+from torchvision.datasets import VOCSegmentation
 from torch.utils.data import DataLoader
-from torchvision.datasets import OxfordIIITPet
 import torchvision.transforms as T
 from PIL import Image
-from sklearn.model_selection import train_test_split
 
-class OxfordPetsDataset(torch.utils.data.Dataset):
-    def __init__(self, root, mode='train', distortion=None):
-        full_dataset = OxfordIIITPet(root=root, split='trainval', target_types='segmentation', download=True)
-        self.distortion = distortion
+# 选定的VOC类别索引
+VOC_TARGET_CLASSES = {
+    15: 1,  # person
+    8: 2,   # cat
+    12: 3,  # dog
+    7: 4    # car
+}
+# 其他类别都设为 ignore_index=255
 
-        # 手动划分 train/test
-        idx_list = list(range(len(full_dataset)))
-        train_idx, test_idx = train_test_split(idx_list, test_size=0.2, random_state=42)
+class VOCSubsetDataset(torch.utils.data.Dataset):
+    def __init__(self, root, image_set='train', download=True):
+        self.dataset = VOCSegmentation(root=root, year='2012', image_set=image_set, download=download)
 
-        if mode == 'train':
-            self.selected_idx = train_idx
-        elif mode == 'val' or mode == 'test':
-            self.selected_idx = test_idx
-        else:
-            raise ValueError("mode must be 'train', 'val' or 'test'")
-
-        self.dataset = full_dataset
-
-        self.input_transform_list = [
+        self.input_transform = T.Compose([
             T.Resize((256, 256)),
-            T.RandomHorizontalFlip(p=0.5),
-        ]
-
-        self.input_transform_list += [
+            T.RandomHorizontalFlip(),
             T.ToTensor(),
             T.Normalize(mean=[0.485, 0.456, 0.406],
                         std=[0.229, 0.224, 0.225])
-        ]
-
-        self.input_transform = T.Compose(self.input_transform_list)
+        ])
 
     def __len__(self):
-        return len(self.selected_idx)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        real_idx = self.selected_idx[idx]
-        img, mask = self.dataset[real_idx]
-
+        img, mask = self.dataset[idx]
         img = self.input_transform(img)
 
         mask = mask.resize((256, 256), Image.NEAREST)
         mask = np.array(mask)
-        mask = torch.from_numpy(mask).long()
 
-        return img, mask
+        # 重新映射mask
+        remapped_mask = np.full_like(mask, fill_value=255)  # 默认ignore
+        for voc_class, mapped_class in VOC_TARGET_CLASSES.items():
+            remapped_mask[mask == voc_class] = mapped_class
+        remapped_mask[mask == 0] = 0  # 背景保留为0
 
-def get_loader(root, batch_size=8, mode='train', shuffle=True, num_workers=2, distortion=None):
-    dataset = OxfordPetsDataset(root=root, mode=mode, distortion=distortion)
+        remapped_mask = torch.from_numpy(remapped_mask).long()
+
+        return img, remapped_mask
+
+def get_loader(root, batch_size=16, mode='train', shuffle=True, num_workers=2):
+    dataset = VOCSubsetDataset(root=root, image_set='train' if mode=='train' else 'val', download=True)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=(mode=='train'), num_workers=num_workers)
     return loader
+
 
 
 
