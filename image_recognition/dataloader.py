d01224ae59fcfc435c5b0487ef7492f1820d3961
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-26 21:40:19 -0400

    Update dataloader.py

--- image_recognition/dataloader.py
+++ image_recognition/dataloader.py
@@ -12,25 +12,26 @@ class CIFAR10Dataset(Dataset):
         self.mode = mode
 
         if mode == 'train':
-            # 读取data_batch_1到data_batch_4
+            # 读取 data_batch_1 ~ data_batch_4
             for i in range(1, 5):
                 batch_file = os.path.join(data_path, f'data_batch_{i}')
                 self._load_batch(batch_file)
         elif mode == 'val':
-            # 读取validation_batch
+            # 读取 validation_batch
             batch_file = os.path.join(data_path, 'validation_batch')
             self._load_batch(batch_file)
         elif mode == 'test':
-            # 读取test_batch
+            # 读取 test_batch
             batch_file = os.path.join(data_path, 'test_batch')
             self._load_batch(batch_file)
         else:
             raise ValueError(f"Unsupported mode {mode}")
 
-        self.data = np.array(self.data)
+        # 所有数据加载完成后，堆叠成一个大的array
+        self.data = np.vstack(self.data)
         self.labels = np.array(self.labels)
 
-        # 定义transform
+        # 定义不同模式下的transform
         if self.mode == 'train':
             self.transform = transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
@@ -44,18 +45,17 @@ class CIFAR10Dataset(Dataset):
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ])
 
-def _load_batch(self, batch_file):
-    with open(batch_file, 'rb') as f:
-        batch = pickle.load(f, encoding='bytes')
-        data = batch[b'data']
-        labels = batch[b'labels']
+    def _load_batch(self, batch_file):
+        with open(batch_file, 'rb') as f:
+            batch = pickle.load(f, encoding='bytes')
+            data = batch[b'data']
+            labels = batch[b'labels']
 
-        data = data.reshape(-1, 3, 32, 32)  # 还原成(N, C, H, W)格式
-        data = np.transpose(data, (0, 2, 3, 1))  # 转成(N, H, W, C)
+            data = data.reshape(-1, 3, 32, 32)  # (N, C, H, W)
+            data = np.transpose(data, (0, 2, 3, 1))  # (N, H, W, C)
 
-        self.data.append(data)
-        self.labels.extend(labels)
-
+            self.data.append(data)
+            self.labels.extend(labels)
 
     def __len__(self):
         return len(self.labels)
@@ -75,4 +75,5 @@ def get_loader(data_path, batch_size, mode='train', shuffle=True, num_workers=2)
         num_workers=num_workers
     )
     return dataloader
+
 
