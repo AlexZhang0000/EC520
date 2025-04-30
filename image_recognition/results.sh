cf3cb53f295695392f28d6bd0613745e3599ecf0
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-30 12:02:49 -0400

    Update results.sh

--- image_recognition/results.sh
+++ image_recognition/results.sh
@@ -11,19 +11,19 @@ echo "🟩 TRAINING PHASE"
 # python train.py --train_distortion gaussianblur:9,4.0
 
 # 3. Train Gaussian Noise models
-python train.py --train_distortion gaussiannoise:0,0.02
-python train.py --train_distortion gaussiannoise:0,0.1
-python train.py --train_distortion gaussiannoise:0,0.25
+# python train.py --train_distortion gaussiannoise:0,0.02
+# python train.py --train_distortion gaussiannoise:0,0.1
+# python train.py --train_distortion gaussiannoise:0,0.25
 
 # 4. Train Aliasing models
-python train.py --train_distortion aliasing:2
-python train.py --train_distortion aliasing:4
-python train.py --train_distortion aliasing:8
+# python train.py --train_distortion aliasing:2
+# python train.py --train_distortion aliasing:4
+# python train.py --train_distortion aliasing:8
 
 # 5. Train JPEG Compression models
-python train.py --train_distortion jpegcompression:60
-python train.py --train_distortion jpegcompression:20
-python train.py --train_distortion jpegcompression:5
+# python train.py --train_distortion jpegcompression:60
+# python train.py --train_distortion jpegcompression:20
+# python train.py --train_distortion jpegcompression:5
 
 echo "🟦 TESTING PHASE"
 # 1. Test clean model on clean data
